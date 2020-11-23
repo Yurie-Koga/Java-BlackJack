1eac1d8c52dad04c734e91a509fec5d716ce3c2d
YumiMachino <y.y.1515m@gmail.com>
2020-11-23 15:24:41 -0800

    Reformat judge file

--- src/sample/model/Judge.java
+++ src/sample/model/Judge.java
@@ -1,27 +1,29 @@
 package sample.model;
+
 import javafx.scene.control.Alert;
 
 import java.util.ArrayList;
+
 /**
  * This is to judge the end of game: WIN/BUST/PUSH
  */
 public class Judge extends Player {
 
-    public Judge(String name, ArrayList<Card> hand,  int sum){
+    public Judge(String name, ArrayList<Card> hand, int sum) {
         super(name, hand, sum);
     }
 
-
     /**
      * Check if the first 2 cards are BlackJack
+     *
      * @param players
      * @param name
      * @return boolean
      */
-    public static boolean isBlackJack(ArrayList<Player> players, String name){
-        for (int i = 0; i < players.size(); i++){
-            if (players.get(i).getName().equals(name)){
-                if(players.get(i).getSum() == 21){
+    public static boolean isBlackJack(ArrayList<Player> players, String name) {
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getName().equals(name)) {
+                if (players.get(i).getSum() == 21) {
                     return true;
                 }
             }
@@ -29,14 +31,14 @@ public class Judge extends Player {
         return false;
     }
 
-
     /**
      * Check if sum of player's hand exceed 21, and if so disable the buttons
+     *
      * @param player
      * @return boolean
      */
-    public static boolean isBusted(Player player){
-        if (player.getSum() > 21){
+    public static boolean isBusted(Player player) {
+        if (player.getSum() > 21) {
             return true;
         }
         return false;
@@ -44,9 +46,10 @@ public class Judge extends Player {
 
     /**
      * Check the game result
+     *
      * @param players
      */
-    public static void gameResult(ArrayList<Player> players){
+    public static void gameResult(ArrayList<Player> players) {
         System.out.println("game Result method ");
         int player1Sum = 0;
         int player2Sum = 0;
@@ -62,22 +65,22 @@ public class Judge extends Player {
             }
         }
 
-        if (dealerSum > 21){
+        if (dealerSum > 21) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Dealer Bust");
             alert.setHeaderText("Dealer Busted! Player win!");
             alert.showAndWait();
             System.exit(0);
         }
-        if (dealerSum <= 21 && player1Sum <= 21 && player1Sum != 0){
+        if (dealerSum <= 21 && player1Sum <= 21 && player1Sum != 0) {
             int dealerDiff = 21 - dealerSum;
             int player1Diff = 21 - player1Sum;
-            if (dealerDiff == player1Diff){
+            if (dealerDiff == player1Diff) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("PUSH");
                 alert.setHeaderText("Dealer and Player1 Push!");
                 alert.showAndWait();
-            } else if (dealerDiff < player1Diff){
+            } else if (dealerDiff < player1Diff) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Dealer won");
                 alert.setHeaderText("Dealer won!");
@@ -89,15 +92,15 @@ public class Judge extends Player {
                 alert.showAndWait();
             }
         }
-        if (dealerSum <= 21 && player2Sum <= 21 && player2Sum != 0){
+        if (dealerSum <= 21 && player2Sum <= 21 && player2Sum != 0) {
             int dealerDiff = 21 - dealerSum;
             int player2Diff = 21 - player2Sum;
-            if (dealerDiff == player2Diff){
+            if (dealerDiff == player2Diff) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("PUSH");
                 alert.setHeaderText("Dealer and Player2 Push!");
                 alert.showAndWait();
-            } else if (dealerDiff < player2Diff){
+            } else if (dealerDiff < player2Diff) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Dealer won");
                 alert.setHeaderText("Dealer won!");
